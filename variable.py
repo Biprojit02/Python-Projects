eec9e8937864c328a59691216019319403f4e390
Biprojit02 <98759448+Biprojit02@users.noreply.github.com>
2023-07-06 10:10:36 +0530

    Add files via upload
    Here I explain different types of variable and datatype in python..

---
+++ variable.py
@@ -0,0 +1,18 @@
+a = 5  #integer
+b = 5.5  #float
+c = "Biprojit"  #string
+d = True  #bool
+
+print("datatype of a is ", type(a))
+print("datatype of b is ", type(b))
+print("datatype of c is ", type(c))
+print("datatype of d is ", type(d))
+
+print(a)
+print(b)
+print(d)
+print(c)
+print(a+b)
+
+# we cant't print like this
+### print(a+c);
