710c53f2048948fd83cdd02994705c86e25d99c6
Biprojit02 <98759448+Biprojit02@users.noreply.github.com>
2023-07-06 10:42:30 +0530

    Create Topic4.py

---
+++ Topic4.py
@@ -0,0 +1,20 @@
+# Type Casring
+# there are two type of type casting...
+# --->explicit type casting
+# --->implicit type casting
+
+# explicit type castin
+# Explicit type conversion is performed by the user by explicitly using type conversion functions in the program code
+a = "1"
+b = "2"
+print(int(a)+int(b))
+
+# output --> 3
+
+# implicit type castin
+# Implicit type conversion is performed by a Python interpreter only.
+a = 1
+b = 2.5
+print(a+b);
+
+# output --> 3.5
