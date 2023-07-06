17a9d60198e684a0a1e05d8d4cfb9ee765431d45
Biprojit02 <98759448+Biprojit02@users.noreply.github.com>
2023-07-06 22:48:54 +0530

    Create Topic6.py

---
+++ Topic6.py
@@ -0,0 +1,46 @@
+# FOR LOOP
+name = "Biprojit"
+for i in name:
+    print(i)
+
+for i in range(11):
+    print(i)
+
+for j in range(1, 10):
+    print(j)
+
+for k in range(1, 12, 4):
+    print(k) #1,5,9
+
+#### OUTPUT
+# B
+# i
+# p
+# r
+# o
+# j
+# i
+# t
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 1
+# 5
+# 9
