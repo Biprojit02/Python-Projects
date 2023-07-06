d58860e4aad188c7790fa1519f9189c52b7a5236
Biprojit02 <98759448+Biprojit02@users.noreply.github.com>
2023-07-06 22:34:32 +0530

    Add files via upload

---
+++ 6input.py
@@ -0,0 +1,11 @@
+# Taking input from user
+a = input("Enter your name: ")
+print("My name is: ",a)
+
+b = input("Enter first number: ")
+c = input("Enter second number: ") ####Python treat every value as a String, if you not mention during giving input
+d = input("enter his name: ")
+print(b+c)
+print(int(b)+int(c))
+print(int(b)+int(d))
+
